190bcf6919da92890d7bd1a1e773acc230e4145e
avanine <marike.puranen@gmail.com>
2021-12-12 17:20:54 +0200

    react-tiedostot

---
+++ src/client/src/components/Otsikko.js
@@ -0,0 +1,12 @@
+import LisaaNappi from "./LisaaNappi";
+
+function Otsikko() {
+    return (
+        <div className="bg-dark text-secondary px-4 py-5 text-center mb-5">
+        <h1 className="mb-4">Lukuvinkkikirjasto</h1>
+        <LisaaNappi />
+      </div>
+    )
+}
+
+export default Otsikko;
