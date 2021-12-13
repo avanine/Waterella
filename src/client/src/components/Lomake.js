a774a312e884c9b0b6319fdb8e0cfade872e2a63
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-12-13 23:12:41 +0200

    graafisen käyttöliittymän refaktorointi

--- src/client/src/components/Lomake.js
+++ src/client/src/components/Lomake.js
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
 import LisaaNappi from "./LisaaNappi";
 import LomakeInput from "./LomakeInput";
-
 function Lomake() {
+  const [type, setType] = useState('Kirja');
+  const [header, setHeader] = useState('');
+  const [ISBN, setISBN] = useState('');
 
-    const [podcast, setPodcast] = useState(false);
-    const choosePodcast = () => setPodcast(!podcast);
-
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
         e.preventDefault();
         /* tässä pitäis jotenkin lisätä tiedot tietokantaan */
         console.log("lomake lähetetty");
-        console.log(e.target[0].value)
-        console.log(e.target[1].value)
-        document.getElementById("form").reset();
+        console.log(type)
+        console.log(header)
+        {type==='Kirja' && console.log(ISBN)}
     }
+  const onChange = (target) => {
+      setType(target.value);
+    };
 
     return (
         <div className="container col-md-4 col-md-offset-4">
             <form onSubmit={handleSubmit} className="mt-5" id="form">
                 <div className="form-group py-2">
                     <label htmlFor="tyyppi">Vinkin tyyppi</label>
-                    <select className="form-control" id="tyyppi">
-                        <option>Kirja</option>
-                        <option onClick={choosePodcast}>Podcast</option>
+                  <select onChange={({ target}) => onChange(target)} className="form-control" id="tyyppi">
+                        <option value="Kirja">Kirja</option>
+                        <option value="Podcast" >Podcast</option>
                     </select>
                 </div>
 
@@ -31,17 +33,16 @@ function Lomake() {
                     label="otsikko"
                     id="otsikko"
                     placeholder="Otsikko"
+                    setValue={setHeader}
+                    value={header}
                 />
-                <LomakeInput
-                    label="isbn"
-                    id="isbn"
-                    placeholder="ISBN"
-                />
-                {podcast && (
+                {type==='Kirja' && (
                     <LomakeInput
                         label="isbn"
                         id="isbn"
                         placeholder="ISBN"
+                        setValue={setISBN}
+                        value={ISBN}
                     />
                 )}
                 <div className="form-group py-2">
