d916024119136d7a78bad73dde0439fdc8958313
Joona <joona.kauranen@gmail.com>
2021-12-16 18:24:35 +0200

    .features

--- src/test/java/Stepdefs.java
+++ src/test/java/Stepdefs.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 public class Stepdefs {
     Kirja vinkki;
+    Podcast podcast;
     
     @Given("Kirja luodaan nimellä Tuoteseloste")
     public void setUp() {
@@ -14,6 +15,27 @@ public class Stepdefs {
     @Then("kirjan nimen tulee olla Tuoteseloste")
     public void konstruktoriLuoOikeanOtsikkosenLukuvinkin() {
         assertEquals("Tuoteseloste", vinkki.getOtsikko());
+    }
+
+    @When("kuvaus 'kirjan kuvaus' lisätään")
+    public void setKuvausTest() {
+        vinkki.setKuvaus("kirjan kuvaus");
+        assertEquals("kirjan kuvaus", vinkki.getKuvaus());
+    }
+
+    @Then("kirjan kuvaus tulee olla 'kirjan kuvaus'")
+    public void getKuvausTest() {
+        assertEquals("kirjan kuvaus", vinkki.getKuvaus());
+    }
+
+    @Given("Kirja luodaan nimellä Tuoteseloste")
+    public void setUpPodcast() {
+        podcast = new Podcast("podcast");
+    }
+
+    @Then("kirjan nimen tulee olla Tuoteseloste")
+    public void konstruktoriLuoOikeanOtsikkosenPodcastin() {
+        assertEquals("podcast", podcast.getOtsikko());
     }   
 
 }
