5e20fe3dfe1fad64665446a050edfc0b6e710871
Joel6677 <joel.heusala@gmail.com>
2021-12-17 20:02:31 +0200

    main takaisin

---
+++ src/main/java/Vinkit.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vinkit {
+    List<LukuvinkkiInterface> vinkit;
+
+    public Vinkit() {
+        vinkit = new ArrayList<>();
+    }
+
+    public void lisaa(LukuvinkkiInterface vinkki) {
+        vinkit.add(vinkki);
+    }
+
+    public String listaaVinkit() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < vinkit.size(); i++) {
+            sb.append(vinkit.get(i));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
